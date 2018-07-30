357b23d1201bf65207bf42dd61e385e42e1a6903
KimReady <kjb4380@gmail.com>
2018-07-30 19:19:28 +0900

    add getter&setter method in Time class

--- app/src/main/java/com/example/ready/studytimemanagement/model/Time.java
+++ app/src/main/java/com/example/ready/studytimemanagement/model/Time.java
@@ -14,6 +14,14 @@ public class Time {
         data_list.add(new_data);
     }
 
+    public ArrayList<Data> getData_list() {
+        return data_list;
+    }
+
+    public void setData_list(ArrayList<Data> data_list) {
+        this.data_list = data_list;
+    }
+
     public void setTotal_time(String total_time) {
         this.total_time = total_time;
     }
