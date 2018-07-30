b80fc5b668b4b5d2c95a096a07f1901d61d3a10d
KimReady <kjb4380@gmail.com>
2018-07-30 17:38:04 +0900

    Add read Json Time data
    read & parse time data received from server

--- app/src/main/java/com/example/ready/studytimemanagement/presenter/ReadJSON.java
+++ app/src/main/java/com/example/ready/studytimemanagement/presenter/ReadJSON.java
@@ -3,6 +3,7 @@ package com.example.ready.studytimemanagement.presenter;
 import android.util.JsonReader;
 import android.util.Log;
 
+import com.example.ready.studytimemanagement.model.Data;
 import com.example.ready.studytimemanagement.model.User;
 
 import java.io.IOException;
@@ -19,17 +20,6 @@ public class ReadJSON {
         } finally {
             reader.close();
         }
-    }
-
-    public List<User> readUserArray(JsonReader reader) throws IOException {
-        List<User> users = new ArrayList<User>();
-
-        reader.beginObject();
-        while (reader.hasNext()) {
-            users.add(readUser(reader));
-        }
-        reader.endArray();
-        return users;
     }
 
     public User readUser(JsonReader reader) throws IOException {
@@ -53,5 +43,46 @@ public class ReadJSON {
         reader.endObject();
         Log.d("json user id", user.getId());
         return user;
+    }
+
+    public List<Data> readJsonTime(InputStream in) throws IOException {
+        JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
+        try {
+            return readTimeArray(reader);
+        } finally {
+            reader.close();
+        }
+    }
+
+    public List<Data> readTimeArray(JsonReader reader) throws IOException {
+        List<Data> time_list = new ArrayList<Data>();
+
+        reader.beginObject();
+        while (reader.hasNext()) {
+            time_list.add(readTime(reader));
+        }
+        reader.endArray();
+        return time_list;
+    }
+
+    public Data readTime(JsonReader reader) throws IOException {
+        Data time = new Data();
+
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            if (name.equals("category")) {
+                time.setCategory(reader.nextString());
+            } else if (name.equals("date")) {
+                time.setDate(reader.nextString());
+            } else if (name.equals("amount")) {
+                time.setAmount(reader.nextString());
+            } else {
+                reader.skipValue();
+            }
+        }
+        reader.endObject();
+        Log.d("json time category", time.getCategory());
+        return time;
     }
 }
