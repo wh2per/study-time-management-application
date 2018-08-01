3aae8c3cad60056c7871ea5f028705233e625c75
KimReady <kjb4380@gmail.com>
2018-08-01 16:23:52 +0900

    refactoring package

--- app/src/main/java/com/example/ready/studytimemanagement/control/ClassifyData.java
+++ app/src/main/java/com/example/ready/studytimemanagement/control/ClassifyData.java
@@ -1,14 +1,11 @@
-package com.example.ready.studytimemanagement.presenter;
+package com.example.ready.studytimemanagement.control;
 
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.util.Log;
 
 import com.example.ready.studytimemanagement.model.AnalysisData;
 import com.example.ready.studytimemanagement.model.Data;
 
 import java.sql.Time;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -24,13 +21,22 @@ public class ClassifyData {
     public ClassifyData() {
         sample = new AnalysisData();
         sample.getAnalysis_category().add(new Data("영어", "2018-07-31", "01:03:05", "03:03:03"));
-        sample.getAnalysis_category().add(new Data("영어", "2018-07-31", "01:03:05", "03:03:03"));
-        sample.getAnalysis_category().add(new Data("영어", "2018-07-31", "01:03:05", "03:03:03"));
+        sample.getAnalysis_category().add(new Data("영어", "2018-07-26", "01:03:05", "03:03:03"));
+        sample.getAnalysis_category().add(new Data("영어", "2018-07-22", "01:03:05", "03:03:03"));
+        sample.getAnalysis_category().add(new Data("국어", "2018-07-01", "01:03:05", "03:03:03"));
         sample.getAnalysis_category().add(new Data("국어", "2018-07-31", "01:03:05", "03:03:03"));
-        sample.getAnalysis_category().add(new Data("국어", "2018-07-31", "01:03:05", "03:03:03"));
+        sample.getAnalysis_period().add(new Data("영어", "2018-07-31", "01:03:05", "03:03:03"));
+        sample.getAnalysis_period().add(new Data("영어", "2018-07-26", "01:03:05", "03:03:03"));
+        sample.getAnalysis_period().add(new Data("영어", "2018-07-22", "01:03:05", "03:03:03"));
+        sample.getAnalysis_period().add(new Data("국어", "2018-07-01", "01:03:05", "03:03:03"));
+        sample.getAnalysis_period().add(new Data("국어", "2018-07-31", "01:03:05", "03:03:03"));
     }
 
+    /**
+     * @brief method to classify time data by Category
+     */
     public void classifyByCategory() {
+        //TODO : change sample data to user's time data
         ArrayList<Data> temp = sample.getAnalysis_category();
         HashMap<String, Long> classification = new HashMap<>();
         Iterator<String> mapIt;
@@ -57,4 +63,9 @@ public class ClassifyData {
         }
     }
 
+    /**
+     * @brief method to classify time data by Period
+     */
+    public void classifyByPeriod() {
+    }
 }
